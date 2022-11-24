6406453313a050b00368651fdb597bb152d5c299
Satya Patel <satyapatel111@gmail.com>
2022-11-24 15:43:05 -0800

    Some structure changes

---
+++ packages/design-system/components/Button/Button.stories.tsx
@@ -0,0 +1,10 @@
+import { ComponentMeta } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Button from './Button';
+
+export default {
+    title: 'Design System/Button',
+    component: Button,
+} as ComponentMeta<typeof Button>;
+
+export const Default = () => <Button onClick={action('Clicked button')} />;
